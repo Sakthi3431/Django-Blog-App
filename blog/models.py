81679baba2e310dfe18906cadb8871d1517701f8
supersakthi0404@gmail.com <@super sakthi123>
2025-08-17 23:43:56 -0700

    added default image

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from django.templatetags.static import static
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    file = models.ImageField(upload_to='blog_images/', default='blog/assets/default.jpg')
+    file = models.ImageField(upload_to='blog_images/', blank=True, null=True, default='blog_images/default.jpg')
     
     def __str__(self):
         return self.title
+
+    @property
+    def file_url(self):
+        if self.file:                 # Safe: False when there is no file
+            try:
+                return self.file.url  # Works only if a file exists
+            except ValueError:
+                pass                  # In case a weird empty value slipped in
+        return static('blog/assets/default.jpg')
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.Home, name='home'), 
+    path('', views.Home, name='home'), 
     path('new/', views.create_blog_post, name='create_post'),
 ]
