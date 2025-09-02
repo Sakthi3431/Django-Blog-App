6057d9d8fafd35856228e9e3d69611cb20fe4859
supersakthi0404@gmail.com <@super sakthi123>
2025-09-02 02:07:02 -0700

    fix: added home btn in profile page and bio word wrap

--- user/models.py
+++ user/models.py
@@ -23,7 +23,7 @@ class Registration(models.Model):
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True, max_length=100)
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='blog_images/user (1).png')
 
     def __str__(self):
